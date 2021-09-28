bfdc22d78969c8a414783ceb64f546d9590ab1ed
jaspercrew <justinph2013@gmail.com>
2021-09-27 20:34:50 -0700

    menu canvas (still needs refreshing )

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private SpriteRenderer _renderer;
     // Start is ca lled before the first frame update
     private void Start() {
         GameObject.FindGameObjectWithTag("Menu Canvas").layer = 5;
+        _renderer = GameObject.FindGameObjectWithTag("Foreground").GetComponent<SpriteRenderer>();
 
     }
 
@@ -26,6 +28,35 @@ public class GameManager : MonoBehaviour
             StartCoroutine(FindObjectOfType<SpawnScript>().SpawnCharacter());
         }
         
+    }
+    public void GamePause() {
+        Time.timeScale = 0;
+        _renderer.enabled = true;
+    }
+
+    public void GameResume() {
+        Time.timeScale = 1;
+        _renderer.enabled = false;
+    }
+
+    public void LoadMainMenu() {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 
     public void LoadLevel(int i)
