15de633dae2bb5e5c5be3dbac7a8c0abfcace93c
burgerhex <aviv.shai@gmail.com>
2021-09-28 22:20:19 -0700

    Fix gliding bug

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -27,6 +27,7 @@ public class CharController: MonoBehaviour {
     // private RewindManager _rewindManager;
     public float xDir = 2;
 
+    private bool _justJumped;
 
     private readonly HashSet<Collider2D> _colliding = new HashSet<Collider2D>();
 
@@ -65,47 +66,16 @@ public class CharController: MonoBehaviour {
         
         xDir = move;
 
-        // if (isMovementEnabled) {
-        //     float move = Input.GetAxis("Horizontal");
-        //     transform.position += new Vector3(move * speed * Time.deltaTime, 0, 0);
-        //     
-        // }
     }
 
     private bool IsGrounded()
     {
         return _colliding.Count > 0;
-        // int playerLayer = 9;
-        // int layerMask = ~(1 << playerLayer);
-        //
-        //
-        // RaycastHit2D hit1 = Physics2D.Raycast(transform.position + Vector3.left * 0.44f, 
-        //     Vector3.down, .52f, layerMask);
-        // RaycastHit2D hit2 = Physics2D.Raycast(transform.position + Vector3.right * 0.44f, 
-        //     Vector3.down, .52f, layerMask);
-        // Debug.Log(hit1.collider);
-        //
-        // return ((hit1.collider != null && !hit1.collider.isTrigger) || 
-        //         (hit1.collider != null && !hit2.collider.isTrigger));
     }
-    
 
 
-    void Update()
+    private void Update()
     {
-        // Debug.Log(transform.position);
-        //Debug.Log("{" + string.Join(", ", colliding.Select(c => c.transform.gameObject.name)) + "}");
-        //Debug.Log(IsGrounded());
-        // Debug.Log(_hangCounter);
-        
-        // hang time code
-        // if (IsGrounded()) {
-        //     Debug.Log("RESETTING HANG TIME");
-        //     _hangCounter = hangTime;
-        // }
-        // else {
-        //     _hangCounter -= Time.deltaTime;
-        // }
         
         // jump buffer code
         if (Input.GetButtonDown("Jump")) {
@@ -133,6 +103,7 @@ public class CharController: MonoBehaviour {
             {
                 int dir = _rigidbody.gravityScale < 0 ? -1 : 1;
                 _rigidbody.AddForce(new Vector2(0, dir * jumpForce), ForceMode2D.Impulse);
+                _justJumped = true;
                 _jumpBufferCount = 0;
             }
             // Debug.Log("SETTING HANG TIME TO -1");
@@ -140,10 +111,11 @@ public class CharController: MonoBehaviour {
         }   
         
         // short jump
-        if (Input.GetButtonUp("Jump") && !IsGrounded() && isMovementEnabled) {
+        if (Input.GetButtonUp("Jump") && !IsGrounded() && isMovementEnabled && _justJumped) {
             // _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * .5f); 
             // same thing but more efficient:
             _rigidbody.velocity = Vector2.Scale(_rigidbody.velocity, new Vector2(1f, 0.5f));
+            _justJumped = false;
         }   
     }
 
@@ -163,12 +135,12 @@ public class CharController: MonoBehaviour {
         if (!col.isTrigger && Abs(charX - colX) < Abs(colW) + Abs(charW) - 0.01f && !col.CompareTag("No Ground"))
         {
             _colliding.Add(col);
+            _justJumped = false;
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        Debug.Log("amongus");
         _colliding.Remove(other.collider);
     }
 
